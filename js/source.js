589a51fe045e72509432d18a34e983fe9757f3ac
Ajaykumar M <m1ajay2001@gmail.com>
2022-05-31 21:34:44 +0530

    Update source.js

--- js/source.js
+++ js/source.js
@@ -1,7 +1,6 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
